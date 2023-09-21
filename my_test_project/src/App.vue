5ea073dd016d948f0b462b8fcae82481b5175b2a
mictlantecutli <moisesparra82@hotmail.com>
2023-09-21 12:56:52 -0600

    finish emit

--- my_test_project/src/App.vue
+++ my_test_project/src/App.vue
@@ -2,7 +2,8 @@
   <LifecycleTest v-if="numberOfClicks < 10" :numberOfClicks="numberOfClicks" />
   <Congratulations_user :numberOfClicks="numberOfClicks" />
   <CounterButton @increment="increment" :numberOfClicks="numberOfClicks" />
-  
+  <TimerComponent @tick="onTick" name="Timer 1" :delay="2000" />
+  <TimerComponent @tick="onTick" name="Time 2" :delay="5000" />
   <PeopleList />
 </template>
 
@@ -13,6 +14,7 @@ import CounterButton from './components/CounterButton.vue';
 
 import Congratulations_user from './components/Congratulations.vue';
 import LifecycleTest from './components/LifecycleTest.vue';
+import TimerComponent from './components/Timer.vue';
 
 export default {
   name: 'App',
@@ -21,6 +23,7 @@ export default {
     CounterButton,
     Congratulations_user,
     LifecycleTest,
+    TimerComponent
     
 },
   data(){
@@ -32,6 +35,9 @@ export default {
   methods:{
     increment(){
       this.numberOfClicks += 1
+    },
+    onTick(name){
+      console.log(name + 'Tick!!')
     }
   }
 }
