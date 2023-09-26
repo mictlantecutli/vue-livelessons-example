b613e06dd53a54fe745a020a78b1dd50a63e7e4a
mictlantecutli <moisesparra82@hotmail.com>
2023-09-26 08:13:20 -0600

    finishCondition

--- my_test_project/src/App.vue
+++ my_test_project/src/App.vue
@@ -1,29 +1,16 @@
 <template>
-  <LifecycleTest v-if="numberOfClicks < 10" :numberOfClicks="numberOfClicks" />
-  <Congratulations_user :numberOfClicks="numberOfClicks" />
-  <CounterButton @increment="increment" :numberOfClicks="numberOfClicks" />
-  <TimerComponent @tick="onTick" name="Timer 1" :delay="2000" />
-  <TimerComponent @tick="onTick" name="Time 2" :delay="5000" />
-  <PeopleList />
+  <FlashingMessage message="Hellow friends!!" :delay="500"/>
 </template>
 
 <script>
 
-import PeopleList from './components/PeopleList.vue';
-import CounterButton from './components/CounterButton.vue';
-
-import Congratulations_user from './components/Congratulations.vue';
-import LifecycleTest from './components/LifecycleTest.vue';
-import TimerComponent from './components/Timer.vue';
+import FlashingMessage from './components/FlashingMessage.vue';
 
 export default {
   name: 'App',
   components: {
-    PeopleList,
-    CounterButton,
-    Congratulations_user,
-    LifecycleTest,
-    TimerComponent
+    
+    FlashingMessage
     
 },
   data(){
