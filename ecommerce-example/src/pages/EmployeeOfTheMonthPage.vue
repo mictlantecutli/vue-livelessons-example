6b52be7902319f117dc5f9fe4e8cd1c128f2264a
mictlantecutli <moisesparra82@hotmail.com>
2023-10-09 08:25:44 -0600

    finish mixins topic

--- ecommerce-example/src/pages/EmployeeOfTheMonthPage.vue
+++ ecommerce-example/src/pages/EmployeeOfTheMonthPage.vue
@@ -8,19 +8,12 @@
 <script>
 /*To install axios is into the project in the terminal type: npm install axios*/
 /*Then this is to use instead fetch and it is more direct to make a call. Look in line 21 how to use that*/
-import axios from 'axios'
+/*import axios from 'axios';*/
+import randomPersonMixin from '../mixins/randomPersonMixin';
   export default{
     name: 'EmployeeOfTheMonthPage',
-    data(){
-      return{
-        personData: null,
-      }
-
-    },
-    created(){
-      axios.get('https://randomuser.me/api/')
-      .then(response => this.personData=response.data.results[0])
-    }
+    mixins: [randomPersonMixin],
+    
 
 
   }
